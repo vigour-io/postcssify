84e529a4e60cecb99d1c4e31878bd178725fc4b1
Shawn Inder <shawn@vigour.io>
2016-04-29 18:38:26 -0400

    Added test for inline css map

--- test/bundles.js
+++ test/bundles.js
@@ -59,6 +59,49 @@ test('bundles', function (t) {
   }).pipe(jsws)
 })
 
+test('bundles with inline css maps', function (t) {
+  var jsws = fs.createWriteStream(jsOut)
+
+  var expected = path.join(__dirname, '_files', 'expected')
+  var jsExpected = expected + '.js'
+  var cssExpected = expected + '_inline.css'
+
+  var brow = browserify()
+  brow.transform(postcssify, {
+    out: cssOut,
+    map: true
+  })
+  brow.add(path.join(__dirname, '_files', 'entry.js'))
+  brow.bundle(function onComplete (err) {
+    if (err) {
+      t.fail('browserify errored: ' + err)
+    }
+    // TODO fix the plugin so this `setTimeout` is no longer necessary
+    setTimeout(function () {
+      Promise.all([
+        fs.readFileAsync(jsOut, 'utf8'),
+        fs.readFileAsync(jsExpected, 'utf8'),
+        fs.readFileAsync(cssOut, 'utf8'),
+        fs.readFileAsync(cssExpected, 'utf8')
+      ]).then((contents) => {
+        t.equal(contents[0], contents[1], 'JS bundle')
+        t.equal(contents[2], contents[3], 'CSS bundle with inline map')
+      }).catch((reason) => {
+        console.error('reading files', reason)
+      })
+      .then(() => {
+        return Promise.all([
+          fs.removeAsync(jsOut),
+          fs.removeAsync(cssOut)
+        ])
+      })
+      .then(() => {
+        t.end()
+      })
+    }, 1500)
+  }).pipe(jsws)
+})
+
 test('source maps', function (t) {
   var jsws = fs.createWriteStream(jsOut)
 
