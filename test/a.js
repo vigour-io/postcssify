d3e3715a6fe61c9e26f244813941e3101aa0e5ae
Youri Daamen <me@youz.io>
2016-08-23 13:28:15 +0200

    moved feature/transform to master

--- test/a.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-require('./second.css')
-
-var b = require('./b')
--- test/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-// var example = require('vigour-element/example/collection')
-require('./first.css')
-
-var a = require('./a')
-
-var example = require('vigour-element/example/collection')
